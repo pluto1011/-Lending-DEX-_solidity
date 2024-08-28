3a365dc136bc27de0db598e2ea8e439216e0c09c
icarus <icarus@upside.center>
2024-08-28 18:59:16 +0900

    lets goooooooo

--- test/test1.sol
+++ test/test1.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "openzeppelin-contracts/token/ERC20/ERC20.sol";
-import "../src/Dex.sol";
+import "../src/DEX.sol";
 
 contract CustomERC20 is ERC20 {
     constructor(string memory tokenName) ERC20(tokenName, tokenName) {
@@ -44,7 +44,11 @@ contract DexTest is Test {
         uint256 firstLPReturn = dex.addLiquidity(1000 ether, 1000 ether, 0);
         emit log_named_uint("firstLPReturn", firstLPReturn);
 
-        uint256 secondLPReturn = dex.addLiquidity(1000 ether * 2, 1000 ether * 2, 0);
+        uint256 secondLPReturn = dex.addLiquidity(
+            1000 ether * 2,
+            1000 ether * 2,
+            0
+        );
         emit log_named_uint("secondLPReturn", secondLPReturn);
 
         assertEq(firstLPReturn * 2, secondLPReturn, "AddLiquidity Error 2");
@@ -54,8 +58,13 @@ contract DexTest is Test {
         uint256 firstLPReturn = dex.addLiquidity(1000 ether, 1000 ether, 0);
         emit log_named_uint("firstLPReturn", firstLPReturn);
 
-        (bool success,) = address(dex).call(
-            abi.encodeWithSelector(dex.addLiquidity.selector, 1000 ether, 1000 ether, (firstLPReturn * 10001) / 10000)
+        (bool success, ) = address(dex).call(
+            abi.encodeWithSelector(
+                dex.addLiquidity.selector,
+                1000 ether,
+                1000 ether,
+                (firstLPReturn * 10001) / 10000
+            )
         );
         assertTrue(!success, "AddLiquidity minimum LP return error");
     }
@@ -64,10 +73,19 @@ contract DexTest is Test {
         uint256 firstLPReturn = dex.addLiquidity(1000 ether, 1000 ether, 0);
         emit log_named_uint("firstLPReturn", firstLPReturn);
 
-        (bool success, bytes memory alret) =
-            address(dex).call(abi.encodeWithSelector(dex.addLiquidity.selector, 1000 ether, 4000 ether, 0));
+        (bool success, bytes memory alret) = address(dex).call(
+            abi.encodeWithSelector(
+                dex.addLiquidity.selector,
+                1000 ether,
+                4000 ether,
+                0
+            )
+        );
         uint256 lpret = uint256(bytes32(alret));
-        assertTrue((firstLPReturn == lpret) || !success, "AddLiquidity imbalance add liquidity test error");
+        assertTrue(
+            (firstLPReturn == lpret) || !success,
+            "AddLiquidity imbalance add liquidity test error"
+        );
     }
 
     function testAddLiquidity5() external {
@@ -89,19 +107,53 @@ contract DexTest is Test {
     }
 
     function testAddLiquidity6() external {
-        (bool success,) = address(dex).call(abi.encodeWithSelector(dex.addLiquidity.selector, 0 ether, 0 ether, 0));
-        assertTrue(!success, "AddLiquidity invalid initialization check error - 1");
-        (success,) = address(dex).call(abi.encodeWithSelector(dex.addLiquidity.selector, 1 ether, 0 ether, 0));
-        assertTrue(!success, "AddLiquidity invalid initialization check error - 2");
-        (success,) = address(dex).call(abi.encodeWithSelector(dex.addLiquidity.selector, 0 ether, 1 ether, 0));
-        assertTrue(!success, "AddLiquidity invalid initialization check error - 3");
+        (bool success, ) = address(dex).call(
+            abi.encodeWithSelector(
+                dex.addLiquidity.selector,
+                0 ether,
+                0 ether,
+                0
+            )
+        );
+        assertTrue(
+            !success,
+            "AddLiquidity invalid initialization check error - 1"
+        );
+        (success, ) = address(dex).call(
+            abi.encodeWithSelector(
+                dex.addLiquidity.selector,
+                1 ether,
+                0 ether,
+                0
+            )
+        );
+        assertTrue(
+            !success,
+            "AddLiquidity invalid initialization check error - 2"
+        );
+        (success, ) = address(dex).call(
+            abi.encodeWithSelector(
+                dex.addLiquidity.selector,
+                0 ether,
+                1 ether,
+                0
+            )
+        );
+        assertTrue(
+            !success,
+            "AddLiquidity invalid initialization check error - 3"
+        );
     }
 
     function testRemoveLiquidity1() external {
         uint256 firstLPReturn = dex.addLiquidity(1000 ether, 1000 ether, 0);
         emit log_named_uint("firstLPReturn", firstLPReturn);
 
-        uint256 secondLPReturn = dex.addLiquidity(1000 ether * 2, 1000 ether * 2, 0);
+        uint256 secondLPReturn = dex.addLiquidity(
+            1000 ether * 2,
+            1000 ether * 2,
+            0
+        );
         emit log_named_uint("secondLPReturn", secondLPReturn);
 
         (uint256 tx, uint256 ty) = dex.removeLiquidity(secondLPReturn, 0, 0);
@@ -113,11 +165,20 @@ contract DexTest is Test {
         uint256 firstLPReturn = dex.addLiquidity(1000 ether, 1000 ether, 0);
         emit log_named_uint("firstLPReturn", firstLPReturn);
 
-        uint256 secondLPReturn = dex.addLiquidity(1000 ether * 2, 1000 ether * 2, 0);
+        uint256 secondLPReturn = dex.addLiquidity(
+            1000 ether * 2,
+            1000 ether * 2,
+            0
+        );
         emit log_named_uint("secondLPReturn", secondLPReturn);
 
-        (bool success,) = address(dex).call(
-            abi.encodeWithSelector(dex.removeLiquidity.selector, secondLPReturn, 2001 ether, 2001 ether)
+        (bool success, ) = address(dex).call(
+            abi.encodeWithSelector(
+                dex.removeLiquidity.selector,
+                secondLPReturn,
+                2001 ether,
+                2001 ether
+            )
         );
         assertTrue(!success, "RemoveLiquidity minimum return error");
     }
@@ -126,10 +187,19 @@ contract DexTest is Test {
         uint256 firstLPReturn = dex.addLiquidity(1000 ether, 1000 ether, 0);
         emit log_named_uint("firstLPReturn", firstLPReturn);
 
-        uint256 secondLPReturn = dex.addLiquidity(1000 ether * 2, 1000 ether * 2, 0);
+        uint256 secondLPReturn = dex.addLiquidity(
+            1000 ether * 2,
+            1000 ether * 2,
+            0
+        );
         emit log_named_uint("secondLPReturn", secondLPReturn);
 
-        (bool success,) = address(dex).call(abi.encodeWithSelector(dex.removeLiquidity.selector, secondLPReturn * 2));
+        (bool success, ) = address(dex).call(
+            abi.encodeWithSelector(
+                dex.removeLiquidity.selector,
+                secondLPReturn * 2
+            )
+        );
         assertTrue(!success, "RemoveLiquidity exceeds balance check error");
     }
 
@@ -137,7 +207,11 @@ contract DexTest is Test {
         uint256 firstLPReturn = dex.addLiquidity(1000 ether, 1000 ether, 0);
         emit log_named_uint("firstLPReturn", firstLPReturn);
 
-        uint256 secondLPReturn = dex.addLiquidity(1000 ether * 2, 1000 ether * 2, 0);
+        uint256 secondLPReturn = dex.addLiquidity(
+            1000 ether * 2,
+            1000 ether * 2,
+            0
+        );
         emit log_named_uint("secondLPReturn", secondLPReturn);
 
         uint256 sumX;
@@ -163,8 +237,10 @@ contract DexTest is Test {
 
         (uint256 rx, uint256 ry) = dex.removeLiquidity(firstLPReturn, 0, 0);
 
-        bool successX = rx <= ((poolAmountX * 10001) / 10000 / 3) && rx >= ((poolAmountX * 9999) / 10000 / 3); // allow 0.01%;
-        bool successY = ry <= ((poolAmountY * 10001) / 10000 / 3) && ry >= ((poolAmountY * 9999) / 10000 / 3); // allow 0.01%;
+        bool successX = rx <= ((poolAmountX * 10001) / 10000 / 3) &&
+            rx >= ((poolAmountX * 9999) / 10000 / 3); // allow 0.01%;
+        bool successY = ry <= ((poolAmountY * 10001) / 10000 / 3) &&
+            ry >= ((poolAmountY * 9999) / 10000 / 3); // allow 0.01%;
         assertTrue(successX, "remove liquidity after swap error; rx");
         assertTrue(successY, "remove liquidity after swap error; ry");
     }
@@ -179,15 +255,16 @@ contract DexTest is Test {
         uint256 poolAmountX = 60000 ether + 3000 ether;
         uint256 poolAmountY = 80000 ether + 4000 ether;
 
-        int256 expectedOutput =
-            -(int256(poolAmountX * poolAmountY) / int256(poolAmountX + 300 ether)) + int256(poolAmountY);
+        int256 expectedOutput = -(int256(poolAmountX * poolAmountY) /
+            int256(poolAmountX + 300 ether)) + int256(poolAmountY);
         expectedOutput = (expectedOutput * 999) / 1000; // 0.1% fee
         uint256 uExpectedOutput = uint256(expectedOutput);
 
         emit log_named_int("expected output", expectedOutput);
         emit log_named_uint("real output", output);
 
-        bool success = output <= ((uExpectedOutput * 10001) / 10000) && output >= ((uExpectedOutput * 9999) / 10000); // allow 0.01%;
+        bool success = output <= ((uExpectedOutput * 10001) / 10000) &&
+            output >= ((uExpectedOutput * 9999) / 10000); // allow 0.01%;
         assertTrue(success, "Swap test fail 1; expected != return");
     }
 
@@ -201,15 +278,16 @@ contract DexTest is Test {
         uint256 poolAmountX = 60000 ether + 3000 ether;
         uint256 poolAmountY = 80000 ether + 4000 ether;
 
-        int256 expectedOutput =
-            -(int256(poolAmountY * poolAmountX) / int256(poolAmountY + 6000 ether)) + int256(poolAmountX);
+        int256 expectedOutput = -(int256(poolAmountY * poolAmountX) /
+            int256(poolAmountY + 6000 ether)) + int256(poolAmountX);
         expectedOutput = (expectedOutput * 999) / 1000; // 0.1% fee
         uint256 uExpectedOutput = uint256(expectedOutput);
 
         emit log_named_int("expected output", expectedOutput);
         emit log_named_uint("real output", output);
 
-        bool success = output <= ((uExpectedOutput * 10001) / 10000) && output >= ((uExpectedOutput * 9999) / 10000); // allow 0.01%;
+        bool success = output <= ((uExpectedOutput * 10001) / 10000) &&
+            output >= ((uExpectedOutput * 9999) / 10000); // allow 0.01%;
         assertTrue(success, "Swap test fail 2; expected != return");
     }
 
@@ -219,7 +297,9 @@ contract DexTest is Test {
 
         // y -> x
         // check invalid swap
-        (bool success,) = address(dex).call(abi.encodeWithSelector(dex.swap.selector, 1, 6000 ether, 0));
+        (bool success, ) = address(dex).call(
+            abi.encodeWithSelector(dex.swap.selector, 1, 6000 ether, 0)
+        );
         assertTrue(!success, "Swap test fail 3; invalid input test failed");
     }
 
@@ -231,16 +311,25 @@ contract DexTest is Test {
         uint256 poolAmountX = 60000 ether + 3000 ether;
         uint256 poolAmountY = 80000 ether + 4000 ether;
 
-        int256 expectedOutput =
-            -(int256(poolAmountY * poolAmountX) / int256(poolAmountY + 6000 ether)) + int256(poolAmountX);
+        int256 expectedOutput = -(int256(poolAmountY * poolAmountX) /
+            int256(poolAmountY + 6000 ether)) + int256(poolAmountX);
         expectedOutput = (expectedOutput * 999) / 1000; // 0.1% fee
         uint256 uExpectedOutput = uint256(expectedOutput);
 
         emit log_named_int("expected output", expectedOutput);
 
-        (bool success,) =
-            address(dex).call(abi.encodeWithSelector(dex.swap.selector, 0, 6000 ether, (uExpectedOutput * 1005) / 1000));
-        assertTrue(!success, "Swap test fail 4; minimum ouput amount check failed");
+        (bool success, ) = address(dex).call(
+            abi.encodeWithSelector(
+                dex.swap.selector,
+                0,
+                6000 ether,
+                (uExpectedOutput * 1005) / 1000
+            )
+        );
+        assertTrue(
+            !success,
+            "Swap test fail 4; minimum ouput amount check failed"
+        );
     }
 
     function testAddLiquidity7() external {
